0026dfb95029c13c28d6cbeecace8c0c7cb70dac
acidDrain <james.grow@fishtech.group>
2019-12-28 02:53:12 -0600

    Removed debug lines. Also changed apiToken to PIHOLE_API_TOKEN to be more explicit

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,14 @@ interface IPiholeMain {
   piholeAddress: string;
   elasticHost: string;
   verifySSL?: boolean;
-  apiToken: string;
+  PIHOLE_API_TOKEN: string;
   startTime?: number;
   endTime?: number;
 }
 
 interface IPiholeAPIQURL {
   piholeAddress: string;
-  apiToken: string;
+  PIHOLE_API_TOKEN: string;
   startTime?: number;
   endTime?: number;
 }
@@ -37,40 +37,33 @@ interface IPiholeAPIQURL {
 const createIndexTimestamp = (dateObj: Date): string =>
   `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
 
-const constructQuery = ({ piholeAddress, apiToken, startTime, endTime }: IPiholeAPIQURL): string =>
-  `https://${piholeAddress}/admin/api_db.php?auth=${apiToken}&getAllQueries&from=${startTime}&until=${endTime}&types=1,2,3,4,5,6,7,8`;
+const constructQuery = ({ piholeAddress, PIHOLE_API_TOKEN, startTime, endTime }: IPiholeAPIQURL): string =>
+  `https://${piholeAddress}/admin/api_db.php?auth=${PIHOLE_API_TOKEN}&getAllQueries&from=${startTime}&until=${endTime}&types=1,2,3,4,5,6,7,8`;
 
 export default async ({
-  DEBUG = false,
   piholeAddress,
   elasticHost,
   verifySSL = false,
-  apiToken,
+  PIHOLE_API_TOKEN,
   startTime = (NOW - fifteenMinutes),
   endTime = NOW
 }: IPiholeMain): Promise<Response> => {
-  const piholeURL = constructQuery({piholeAddress, apiToken, startTime, endTime });
-  if (DEBUG) console.log(`piholeURL: ${piholeURL}`);
+  const piholeURL = constructQuery({piholeAddress, PIHOLE_API_TOKEN, startTime, endTime });
 
   const res = await fetch(piholeURL, { agent: new https.Agent({ rejectUnauthorized: verifySSL }) });
+
   const { data } = await res.json();
 
-  if (DEBUG) console.log(`piholeResponse: ${data}`);
-  if (DEBUG) console.log(`endTime: ${endTime}`);
   const TS = new Date(msToS(endTime));
-  if (DEBUG) console.log(`TS: ${TS}`);
+
   const tsString = createIndexTimestamp(TS);
-  if (DEBUG) console.log(tsString);
 
   const piholeIndex = `pihole-logs-${tsString}`;
-  if (DEBUG) console.log(piholeIndex);
 
   const formattedData = makeNdJson(piholeIndex, data.map(formatRecord));
-  if (DEBUG) console.log(`formattedData: ${formattedData}`);
+
   const elasticsearchURL = `https://${elasticHost}/_bulk`;
-  if (DEBUG) console.log(`elasticsearchURL: ${elasticsearchURL}`);
-  
-  if (DEBUG) console.log('Submitting to ES');
+
   return submitToEs({ ndjsonData: formattedData, elasticsearchURL, verifySSL });
 }
 
